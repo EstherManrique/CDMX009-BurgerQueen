425e34b911b87762c9d94119062e5e2e115e0c6b
Esther Manrique <esmango0610@gmail.com>
2020-07-21 13:22:06 -0500

    first test passed

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+import WaiterRegister from './components/WaiterRegister';
+
+/* test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/waiter/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
+
+/* test('renders learn react link', () => {
+  const { getByRole } = render(<App />);
+  expect(screen.getByRole('button')).not.toHaveAttribute('disabled')
+}); */
+
+it('should take a snapshot', () => {
+  const { asFragment } = render(<App />)  
+  expect(asFragment(<App />)).toMatchSnapshot()
+ });
+
+
+
